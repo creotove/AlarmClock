c23624eb6741fd09d723555c8ffa93e11bd9e8e2
creotove <85667227+creotove@users.noreply.github.com>
2023-02-07 18:00:10 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# Alarm Clock
+Can set a Alarm 
+
+# Features
+Turn off alarm with a single click
+User-friendly interface
+
+# Usage
+Visit the website, set the desired alarm time and click the "Set Alarm" button.
+The alarm will go off at the specified time.
+
+# Live Demo
+https://creotove.github.io/AlarmClock/
+
+# Contact
+For any questions or concerns, please open an issue in this repository.
+
+# Aduio used
+I do not own the audio i used th link of the Audio to listen on Youtube is " https://youtube.com/watch?v=ApXoWvfEYVU&si=EnSIkaIECMiOmarE "
---
+++ index.js
@@ -0,0 +1,82 @@
+function bodyLoaded() {
+    let audio = new Audio("sunflower.mp3") // Importing Audio
+    let hour = document.getElementById("hour") // Getting hour from <select>
+    let min = document.getElementById("min") // Getting min from <select>
+    let ampm = document.getElementById("ampm") // Getting ampm from <select>
+    let antiPrime = document.getElementById("antiPrime") // Getting AM/PM from the Current Time
+
+    const btn = document.createElement("input"); // Creating a <input> that will stop the Audio of the Alaram
+    btn.setAttribute("type", "submit") // Setting Attribute i.e. <input type="submit">
+    btn.setAttribute("value", "Off Alarm") // Setting Attribute i.e. <input type="submit" value="Off Alarm">
+    btn.setAttribute("id", "offAlarm") // Setting Attribute i.e. <input type="submit" value="Off Alaram" id="offAlarm">
+
+
+
+    setInterval(() => { // SetInterval for displaying the current Time that will continously change after 1000ms
+        let time = new Date(); // Creating an object of Date
+        let H = document.getElementById("H") // Area where current Hour will be shown
+        let M = document.getElementById("M") // Area where current Minutes will be shown
+        let S = document.getElementById("S") // Area where current Seconds will be shown
+
+        currHour = Math.floor(time.getHours() % 12) // Conveting time format which is bydefaut 24 to 12 and storing it
+        currMin = time.getMinutes() // Getting Current Minutes to be displayed
+        currSec = time.getSeconds() // Getting Current Seconds to be displayed
+
+        H.innerText = currHour 
+        M.innerText = currMin 
+        S.innerText = currSec
+        if ((time.getHours()) > 12) // Logic for checking whether it is AM or PM. As the default format of getHours is 24 so
+        {
+            antiPrime.innerText = "PM" // we are checking if the current hour is Greater Than 12 then it is PM
+        }
+        else {
+            antiPrime.innerText = "AM" // else PM
+        }
+        if(currHour<12)
+        {
+            H.innerText = `0${currHour}` // Displaying Current Hour with appending zero before it if currHour is Less
+        }                                // Than 12
+        
+        if(currMin<10)
+        {
+            M.innerText = `0${currMin}` // Displaying Current Minutes with appending zero before it if currMin is Less
+        }                                // Than 10
+        if(currSec<10)
+        {
+            S.innerText = `0${currSec}` // Displaying Current Seconds with apending zero before itif currSec is Less
+        }                                // Than 10        
+    }, 1000)
+
+    let setAlarm = document.getElementById("setAlarm"); // Getting the button to Set Alarm
+    setAlarm.addEventListener("click", (e) => { // Adding click Event to it
+        let mint = min.value // Setting Minutes value which get from <select> on selecting them
+        let hours = hour.value // Setting Hours value which get from <select> on selecting them
+        let ap = ampm.value // Setting AM or PM value which get from <select> on selecting them
+
+        if (mint == "Minutes" && hours == "hour") { //If user has not selected time from <select>
+            alert("please select Hours and Minutes :)") // It will give an alert to select it
+
+        }
+        else { //else the alarm will be set
+
+            alert("alarm setted") //alerting  "alarm setted"
+            setInterval(() => { // Creating a setInterval() because that will continuosly check the condition
+                                // at every 2000ms i.e. if user entered hours and minutes are same to the current time or not
+
+                if (H.innerText == hours && M.innerText == mint && antiPrime.innerText == ap) {
+                    audio.play() //if current time and user entered time are matched alarm audio will be played
+                    setAlarm.remove() // Removing Set Alarm Button to display the Off Alarm button
+                    let mainContent = document.getElementById("main-content"); // Getting Id of container where Off Alarm button to be displayed
+                    mainContent.appendChild(btn) // Adding or append the Off Alarm Button that we created at the Starting
+                    btn.addEventListener("click", () => { // Adding click event to stop or pause the alarm audio
+                        audio.pause()
+                        // alert("if you want to set alarm Again please Refresh the Page :))")
+                    })
+                }
+                e.preventDefault()
+            }, 2000)
+        }
+
+    })
+
+}
